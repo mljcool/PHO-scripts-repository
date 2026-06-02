14284cb12043ea0fe9d71d070488aa59fe019c17
Mark Gocela <MGocela+HSO@hso.com>
2026-06-02 15:17:48 +0800

    nav_specialty_js.js

--- Dynamics365_V9_JS_scripts/nav_specialty_js.js
+++ Dynamics365_V9_JS_scripts/nav_specialty_js.js
@@ -1,16 +1,22 @@
-function SetSpecialtyDesc(context) {
-    var oField = context.getEventSource().getValue();
+function SetSpecialtyDesc(executionContext) {
+    var formContext = getFormContext(executionContext);
+    if (!formContext) {
+        return;
+    }
+
+    var eventSource = executionContext ? executionContext.getEventSource() : null;
+    var oField = eventSource ? eventSource.getValue() : null;
     if (typeof (oField) != "undefined" && oField != null) {
-        getAllSpecialitiesDesc(oField[0].id, "nav_allspecialtiesSet");
+        getAllSpecialitiesDesc(formContext, oField[0].id, "nav_allspecialtiesSet");
     }
 }
 
-function getAllSpecialitiesDesc(id, odataSetName) {
+function getAllSpecialitiesDesc(formContext, id, odataSetName) {
     id = id.replace("{", "");
     id = id.replace("}", "");
 
     // Get Server URL
-    var serverUrl = GetServerUrl();
+    var serverUrl = Xrm.Utility.getGlobalContext().getClientUrl();
 
     //The OData end-point
     var ODATA_ENDPOINT = "/XRMServices/2011/OrganizationData.svc";
@@ -26,7 +32,7 @@ function getAllSpecialitiesDesc(id, odataSetName) {
             XMLHttpRequest.setRequestHeader("Accept", "application/json");
         },
         success: function (data, textStatus, XmlHttpRequest) {
-            setDescValue(data, textStatus, XmlHttpRequest)
+            setDescValue(formContext, data, textStatus, XmlHttpRequest)
         },
         error: function (XmlHttpRequest, textStatus, errorThrown) {
             alert("Error – " + errorThrown)
@@ -34,18 +40,51 @@ function getAllSpecialitiesDesc(id, odataSetName) {
     });
 }
 
-function setDescValue(data, textStatus, XmlHttpRequest) {
+function setDescValue(formContext, data, textStatus, XmlHttpRequest) {
     if (data && data.d != null) {
-        Xrm.Page.getAttribute("nav_description").setValue(data.d.nav_description); 
-        Xrm.Page.getAttribute("nav_description").setSubmitMode("always");
-        Xrm.Page.ui.controls.get("nav_description").setDisabled(true);       
+        var descriptionAttr = formContext.getAttribute("nav_description");
+        var descriptionControl = formContext.getControl("nav_description");
+
+        if (descriptionAttr != null) {
+            descriptionAttr.setValue(data.d.nav_description);
+            descriptionAttr.setSubmitMode("always");
+        }
+
+        if (descriptionControl != null) {
+            descriptionControl.setDisabled(true);
+        }
     }
 }
 
-function setDescDisable() {
-    var formType = Xrm.Page.ui.getFormType();
+function setDescDisable(executionContext) {
+    var formContext = getFormContext(executionContext);
+    if (!formContext) {
+        return;
+    }
+
+    var formType = formContext.ui.getFormType();
     if (formType != null && formType != 1) {
-        Xrm.Page.getAttribute("nav_description").setSubmitMode("always");
-        Xrm.Page.ui.controls.get("nav_description").setDisabled(true); 
+        var descriptionAttr = formContext.getAttribute("nav_description");
+        var descriptionControl = formContext.getControl("nav_description");
+
+        if (descriptionAttr != null) {
+            descriptionAttr.setSubmitMode("always");
+        }
+
+        if (descriptionControl != null) {
+            descriptionControl.setDisabled(true);
+        }
      }
  }
+
+function getFormContext(executionContext) {
+    if (executionContext && typeof executionContext.getFormContext === "function") {
+        return executionContext.getFormContext();
+    }
+
+    if (executionContext && typeof executionContext.getAttribute === "function" && typeof executionContext.getControl === "function") {
+        return executionContext;
+    }
+
+    return null;
+}
