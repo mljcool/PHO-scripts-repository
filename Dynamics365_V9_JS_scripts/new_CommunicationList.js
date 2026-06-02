48d8fd8e074cf311d662115d356bf572cdcd064a
Mark Gocela <MGocela+HSO@hso.com>
2026-06-02 15:36:03 +0800

    new_CommunicationList.js

--- Dynamics365_V9_JS_scripts/new_CommunicationList.js
+++ Dynamics365_V9_JS_scripts/new_CommunicationList.js
@@ -5,15 +5,36 @@ if (typeof (de_CommunicationList) == "undefined")
 
 de_CommunicationList.Library = {
     //--------------------------------------------------------------------------
-    onLoad: function () {
-        if (Xrm.Page.ui.getFormType() == 1) {
-            Xrm.Page.getAttribute("createdfromcode").setValue("2");
+    onLoad: function (executionContext) {
+        var formContext = de_CommunicationList.Library.getFormContext(executionContext);
+        if (!formContext) {
+            return;
+        }
+
+        if (formContext.ui.getFormType() == 1) {
+            var createdFromCodeAttr = formContext.getAttribute("createdfromcode");
+            if (createdFromCodeAttr != null) {
+                createdFromCodeAttr.setValue(2);
+            }
         }
     },
 
     //--------------------------------------------------------------------------
-    onSave: function () {
+    onSave: function (executionContext) {
 
+    },
+
+    //--------------------------------------------------------------------------
+    getFormContext: function (executionContext) {
+        if (executionContext && typeof executionContext.getFormContext === "function") {
+            return executionContext.getFormContext();
+        }
+
+        if (executionContext && typeof executionContext.getAttribute === "function" && typeof executionContext.getControl === "function") {
+            return executionContext;
+        }
+
+        return null;
     },
 
     //--------------------------------------------------------------------------
