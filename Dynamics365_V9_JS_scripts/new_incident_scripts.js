9b8eb18cb3a37df6d3655f0c45b68708b4f77280
Mark Gocela <MGocela+HSO@hso.com>
2026-06-02 15:59:44 +0800

    new_incident_scripts.js

--- Dynamics365_V9_JS_scripts/new_incident_scripts.js
+++ Dynamics365_V9_JS_scripts/new_incident_scripts.js
@@ -1,15 +1,37 @@
-function ResolveCustom()
+function ResolveCustom(executionContext)
 {
-    if (Xrm.Page.getAttribute("de_timetrackingenabled").getValue() == 1) {
+    var formContext = getFormContext(executionContext);
+    if (!formContext) {
+        return;
+    }
+
+    var timeTrackingEnabledAttr = formContext.getAttribute("de_timetrackingenabled");
+    var totalTimeAttr = formContext.getAttribute("de_totaltime");
+
+    if (timeTrackingEnabledAttr != null && timeTrackingEnabledAttr.getValue() == 1) {
         PHO_de_Common.Library.setRequired("de_totaltime", "required");
 
-        if (Xrm.Page.getAttribute("de_totaltime").getValue() == 0 || Xrm.Page.getAttribute("de_totaltime").getValue() == null) {
+        if (totalTimeAttr == null || totalTimeAttr.getValue() == 0 || totalTimeAttr.getValue() == null) {
             alert("Total Time must be greater than 0");
-            context.getEventArgs().preventDefault();
+            if (executionContext && executionContext.getEventArgs()) {
+                executionContext.getEventArgs().preventDefault();
+            }
         }
         else
             resolve();
     }
     else
         resolve();
+}
+
+function getFormContext(executionContext) {
+    if (executionContext && typeof executionContext.getFormContext === "function") {
+        return executionContext.getFormContext();
+    }
+
+    if (executionContext && typeof executionContext.getAttribute === "function" && typeof executionContext.getControl === "function") {
+        return executionContext;
+    }
+
+    return null;
 }
