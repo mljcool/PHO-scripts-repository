8d3703cab8968a6691981a1674c42d80afee2877
Mark Gocela <MGocela+HSO@hso.com>
2026-06-02 16:28:31 +0800

    new_SpecialtyScript.js

--- Dynamics365_V9_JS_scripts/new_SpecialtyScript.js
+++ Dynamics365_V9_JS_scripts/new_SpecialtyScript.js
@@ -1,10 +1,16 @@
 // JScript source code
 // Update Specialty description field from Specialties
-function relatedSpecialtiesChange() {
-    var lookupItem = new Array();
-    lookupItem = Xrm.Page.getAttribute("nav_specialty").getValue();
+function relatedSpecialtiesChange(executionContext) {
+    var formContext = getFormContext(executionContext);
+    if (!formContext) {
+        return;
+    }
 
-    if (lookupItem[0] != null) {
+    var lookupItem = new Array();
+    var specialtyAttr = formContext.getAttribute("nav_specialty");
+    lookupItem = specialtyAttr != null ? specialtyAttr.getValue() : null;
+
+    if (lookupItem != null && lookupItem[0] != null) {
         var name = lookupItem[0].name;
         var guid = lookupItem[0].id;
         var entType = lookupItem[0].entityType;
@@ -13,7 +19,7 @@ function relatedSpecialtiesChange() {
 
         try {
             //Get entity data;
-            var serverUrl = Xrm.Page.context.getServerUrl(); // "http://oto480dcrmv/PHODev";
+            var serverUrl = Xrm.Utility.getGlobalContext().getClientUrl(); // "http://oto480dcrmv/PHODev";
             var odataSelect = serverUrl + "/XRMServices/2011/OrganizationData.svc/new_specialtiesSet(guid'" + guid + "')";
             // alert("ODATA Select: " + odataSelect.toString());
 
@@ -27,7 +33,10 @@ function relatedSpecialtiesChange() {
                     var org = data.d;
                     //Change form data
                     // alert("TEST: Description: " + org.new_Description);
-                    Xrm.Page.data.entity.attributes.get("nav_description").setValue(org.nav_Description);
+                    var descriptionAttr = formContext.getAttribute("nav_description");
+                    if (descriptionAttr != null) {
+                        descriptionAttr.setValue(org.nav_Description);
+                    }
                 },
                 error: function (XmlHttpRequest, textStatus, errorThrown) {
                     alert('OData Select Failed: ' + odataSelect);
@@ -40,4 +49,16 @@ function relatedSpecialtiesChange() {
             // alert("Error: " + err.description);
         }
     }
+}
+
+function getFormContext(executionContext) {
+    if (executionContext && typeof executionContext.getFormContext === "function") {
+        return executionContext.getFormContext();
+    }
+
+    if (executionContext && typeof executionContext.getAttribute === "function" && typeof executionContext.getControl === "function") {
+        return executionContext;
+    }
+
+    return null;
 }
