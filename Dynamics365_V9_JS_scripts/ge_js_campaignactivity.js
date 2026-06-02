51ed1d626d956862d3693580a8de3dc5d52ddf48
Mark Gocela <MGocela+HSO@hso.com>
2026-06-02 15:11:53 +0800

    ge_js_campaignactivity.js

--- Dynamics365_V9_JS_scripts/ge_js_campaignactivity.js
+++ Dynamics365_V9_JS_scripts/ge_js_campaignactivity.js
@@ -1,15 +1,35 @@
-function SendAllEmailsFlag(){
+function SendAllEmailsFlag(executionContext){
+    var formContext = getFormContext(executionContext);
+    if (!formContext) {
+        return;
+    }
+
 	var d = new Date();
     var n = d.getMilliseconds();
     var s = n.toString();
+    var sendAllEmailsFlagAttr = formContext.getAttribute("ge_sendallemailsflag");
 
-    Xrm.Page.getAttribute("ge_sendallemailsflag").setValue(s);
-    Xrm.Page.getAttribute("ge_sendallemailsflag").setSubmitMode("always");
-	Xrm.Page.data.entity.save();
+    if (sendAllEmailsFlagAttr != null) {
+    	sendAllEmailsFlagAttr.setValue(s);
+    	sendAllEmailsFlagAttr.setSubmitMode("always");
+    }
+    formContext.data.entity.save();
 	
 	pause(3000);
 	
 	alert("All Emails have been sent");
+}
+
+function getFormContext(executionContext) {
+    if (executionContext && typeof executionContext.getFormContext === "function") {
+        return executionContext.getFormContext();
+    }
+
+    if (executionContext && typeof executionContext.getAttribute === "function" && typeof executionContext.getControl === "function") {
+        return executionContext;
+    }
+
+    return null;
 }
 
 function pause(millis){
